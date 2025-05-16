b1507bd87523016eecf1c8130f187115c2b1a86b
dsvshka-1 <dsvshka@gmail.com>
2025-05-16 22:30:49 +0300

    upd

--- build.py
+++ build.py
@@ -11,14 +11,18 @@ def main():
             if filename.endswith(".c"):
                 cpps.append(os.path.relpath(os.path.join(dirpath, filename), current_dir))
     try:
+        cflags = subprocess.check_output(["python3-config", "--cflags"], universal_newlines=True).strip()
+        ldflags = subprocess.check_output(["python3-config", "--ldflags", "--embed"], universal_newlines=True).strip()
         subprocess.run(
             ["gcc", "-std=c11", "-Wall", "-Wextra", "-Werror", "-pedantic", "-O2"]
-            + ["-I", "src"]
-            + cpps + ['main.c']
-            + ["-o", "build"]
+            + ["-fPIC", "-I", "src"]
+            + cflags.split()
+            + cpps
+            + ["-shared", "-o", "dynamical_systems.so"]
+            + ldflags.split()
             ,check=True
         )
-        subprocess.run(["./build"], check=True)
+        # subprocess.run(["./build"], check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
         if e.stdout:
@@ -26,8 +30,7 @@ def main():
         if e.stderr:
             print(f"Stderr:\\n{e.stderr.decode()}")
     finally:
-        if os.path.exists("build"):
-            os.remove("build")
+        pass
 
 if __name__ == "__main__":
     main()
