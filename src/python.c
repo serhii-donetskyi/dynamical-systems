772cb67d534b404e9f5317da73b0fca989321c9a
dsvshka-1 <dsvshka@gmail.com>
2025-05-29 00:46:25 +0300

    package c file

--- src/python.c
+++ src/python.c
@@ -1,27 +1,15 @@
 #include "py_ode.h"
-#include "py_solver.h"
-#include "py_utils.h"
-#include "py_job.h"
+
 // --- Method Table ---
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wcast-function-type"
-#endif
 static PyMethodDef dynamical_systems_methods[] = {
-    {"create_ode_linear", (PyCFunction)py_create_ode_linear, METH_VARARGS | METH_KEYWORDS, "Create a linear ODE object."},
-    {"create_solver_rk4", (PyCFunction)py_create_solver_rk4, METH_VARARGS | METH_KEYWORDS, "Create a Runge-Kutta 4th order ODE solver object."},
-    {"phase_portrait", (PyCFunction)py_phase_portrait, METH_VARARGS | METH_KEYWORDS, "Create a phase portrait of the ODE."},
-    {NULL, NULL, 0, NULL}
+    {NULL, NULL, 0, NULL}  /* Sentinel */
 };
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
 
 // --- Module Definition & Initialization ---
 static PyModuleDef dynamical_systems_module_def = {
     PyModuleDef_HEAD_INIT,
     "dynamical_systems",
-    "Python interface for dynamical_systems C library ODE and Solver components.",
+    "Python interface for dynamical_systems C library ODE components.",
     -1,
     dynamical_systems_methods,
     NULL, NULL, NULL, NULL
@@ -31,7 +19,7 @@ PyMODINIT_FUNC PyInit_dynamical_systems(void) {
     PyObject *m;
 
     if (PyType_Ready(&OdeTypePy) < 0) return NULL;
-    if (PyType_Ready(&SolverTypePy) < 0) return NULL;
+    if (PyType_Ready(&OdeFactoryTypePy) < 0) return NULL;
 
     m = PyModule_Create(&dynamical_systems_module_def);
     if (m == NULL) return NULL;
@@ -43,10 +31,30 @@ PyMODINIT_FUNC PyInit_dynamical_systems(void) {
         return NULL;
     }
 
-    Py_INCREF(&SolverTypePy);
-    if (PyModule_AddObject(m, "Solver", (PyObject *)&SolverTypePy) < 0) {
-        Py_DECREF(&SolverTypePy);
-        Py_DECREF(&OdeTypePy); // Also decref OdeTypePy
+    Py_INCREF(&OdeFactoryTypePy);
+    if (PyModule_AddObject(m, "OdeFactory", (PyObject *)&OdeFactoryTypePy) < 0) {
+        Py_DECREF(&OdeFactoryTypePy);
+        Py_DECREF(&OdeTypePy);
+        Py_DECREF(m);
+        return NULL;
+    }
+
+    // Add parameter type constants
+    if (PyModule_AddStringConstant(m, "NATURAL", "NATURAL") < 0) {
+        Py_DECREF(&OdeFactoryTypePy);
+        Py_DECREF(&OdeTypePy);
+        Py_DECREF(m);
+        return NULL;
+    }
+    if (PyModule_AddStringConstant(m, "INTEGER", "INTEGER") < 0) {
+        Py_DECREF(&OdeFactoryTypePy);
+        Py_DECREF(&OdeTypePy);
+        Py_DECREF(m);
+        return NULL;
+    }
+    if (PyModule_AddStringConstant(m, "REAL", "REAL") < 0) {
+        Py_DECREF(&OdeFactoryTypePy);
+        Py_DECREF(&OdeTypePy);
         Py_DECREF(m);
         return NULL;
     }
