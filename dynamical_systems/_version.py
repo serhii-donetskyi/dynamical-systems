f9248c854489e7cc006e28915c3dc35e7fd65efc
dsvshka-1 <dsvshka@gmail.com>
2025-07-06 01:05:48 +0300

    test

--- dynamical_systems/_version.py
+++ dynamical_systems/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.2.1.dev3+ged8bfe0.d20250705'
-__version_tuple__ = version_tuple = (0, 2, 1, 'dev3', 'ged8bfe0.d20250705')
+__version__ = version = "0.2.1.dev3+ged8bfe0.d20250705"
+__version_tuple__ = version_tuple = (0, 2, 1, "dev3", "ged8bfe0.d20250705")
