f24b91a20f4dfb2545b97203abde5de04643b45d
dsvshka-1 <dsvshka@gmail.com>
2025-07-06 00:25:43 +0300

    linter fixes

--- tests/test_ode_linear.py
+++ tests/test_ode_linear.py
@@ -2,6 +2,7 @@ import pytest
 import random
 from dynamical_systems import Ode
 
+
 def test_ode(ode_factory):
     ode = ode_factory.create(n=2)
     t = random.random()
@@ -10,26 +11,27 @@ def test_ode(ode_factory):
     ode.set_t(t)
     ode.set_x(x)
     ode.set_p(p)
-    assert ode_factory.get_argument_types() == [{'name': 'n', 'type': int}]
-    assert ode_factory.get_name() == 'linear'
+    assert ode_factory.get_argument_types() == [{"name": "n", "type": int}]
+    assert ode_factory.get_name() == "linear"
     assert isinstance(ode, Ode)
     assert ode.get_x_size() == 2
     assert ode.get_p_size() == 4
     assert ode.get_t() == t
     assert ode.get_x() == x
     assert ode.get_p() == p
-    assert ode.get_arguments() == [{'name': 'n', 'value': 2}]
+    assert ode.get_arguments() == [{"name": "n", "value": 2}]
+
 
 def test_errors(ode_factory):
     with pytest.raises(Exception):
         ode_factory.create(n=-1)
     ode = ode_factory.create(n=2)
     with pytest.raises(Exception):
-        ode.set_t('0')
+        ode.set_t("0")
     with pytest.raises(Exception):
-        ode.set_x('00')
+        ode.set_x("00")
     with pytest.raises(Exception):
-        ode.set_p('0000')
+        ode.set_p("0000")
     with pytest.raises(Exception):
         ode.set_x([random.random() for _ in range(ode.get_x_size() - 1)])
     with pytest.raises(Exception):
