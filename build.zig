d33169bd898ce2ef6df8e4a32c7059aa985623fe
serhii-donetskyi <dsvshka@gmail.com>
2025-12-25 07:28:19 +0200

    change command name

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) !void {
 
     // compile the executable
     const exe = b.addExecutable(.{
-        .name = "dynamical_systems",
+        .name = "dynamical-systems",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
